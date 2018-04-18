12c6085be60d02a34ac3747ea907406c73051d52
speedy-software-ch <isai.klein.ik@gmail.com>
2018-04-18 23:47:01 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,13 +4,9 @@
 #include <stdlib.h>
 #include <curl/curl.h>
 #include <json/json.h>
-#include "functions.c"
 
 
 int main() {
-    
-#include "init_config.c"
-
     //Inform user about preferred media player, as to config file
     char preferred_media_player[1000];
     sprintf(preferred_media_player, "say Your preferred media player is %s", M_P);
@@ -22,20 +18,6 @@ int main() {
     system(preferred_webbrowser);
 
     do {
-        
-        //---------------------------------------------------------------------------------------------------------------------
-        
-        //Artificial Intelligence 
-#include "requests.c"
-        //Artificial Intelligence {REQUESTS}
-        
-#include "analysis.c"
-        //Artificial Intelligence {ANALYSIS}
-        
-#include "response.c"
-        //Artificial Intelligence {RESPONSE}
-
-        //---------------------------------------------------------------------------------------------------------------------
 
     } while ((strcmp(str, "stop") != 0));
 
